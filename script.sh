ff2c60aaeadf6bfc4815a9f695af534d52fdc5ad
Ernesto <90586426+ernestojr-7@users.noreply.github.com>
2022-10-10 08:05:49 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,10 +4,10 @@
 #lembrar de dar permissao de execusao ao script: chmod +x <path>/script.sh
 
 echo "Atualizando o servidor..."
+
 apt-get update && apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
-
 
 echo "Baixando e copiando os arquivos da aplicação..."
 
